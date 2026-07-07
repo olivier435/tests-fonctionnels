4c50f8fbc7ab92c631b5f40ccf26fbe244cf9a69
dehez <olivier.dehez1203@gmail.com>
2026-07-07 18:24:03 +0200

    Mise en place de la Fixtures et du .env.test

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,35 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(
+        private readonly UserPasswordHasherInterface $passwordHasher
+    ) {}
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $user->setEmail('user@test.fr');
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword(
+            $this->passwordHasher->hashPassword($user, 'Password123!')
+        );
+
+        $admin = new User();
+        $admin->setEmail('admin@test.fr');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword(
+            $this->passwordHasher->hashPassword($admin, 'Password123!')
+        );
+
+        $manager->persist($user);
+        $manager->persist($admin);
 
         $manager->flush();
     }
